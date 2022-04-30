edb33474da2a099db2336ac1de3d2d395b86aae6
Nghia Nguyen <aihgn224@gmail.com>
2022-04-29 17:45:48 -0700

    finished game

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import MoleContainer from './MoleContainer';
+import './App.css';
+
+function App() {
+  let [score, setScore] = useState(0)
+  for (let i = 0; i < 9 ; i++){
+    hills.push(
+      <MoleContainer
+      key={i}
+      setScore={ setScore }
+      score ={ score } />
+    )
+  }
+    return (
+      <div>
+        {hills}
+      </div>
+    )
+
+  return (
+    <div className="App">
+      <h1>React-a-Mole!</h1>
+      { score }
+      {createMoleHill()}
+    </div>
+  );
+}
+
+export default App;
